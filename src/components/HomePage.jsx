e42a05f7f781450eb7179313b9f33f8a7d068cb3
Anna Trubnikova <trubo4ka911@gmail.com>
2023-03-06 22:35:18 +0200

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,38 +1,65 @@
-import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import "../sass/HomePage.scss";
+import { clearHistory } from '../redux/gameHistorySlice';
+import Modal from './Modal';
+import '../sass/HomePage.scss';
 
 function HomePage() {
-  const balance = useSelector((state) => state.balance.currentBalance);
-
+  const [showHistory, setShowHistory] = useState(false);
+  const dispatch = useDispatch();
+  const balance = useSelector((state) => state.balance.balance);
   const gameHistory = useSelector((state) => state.gameHistory.history);
-  const balanceToDisplay = balance !== null ? balance : 0;
-  console.log(balanceToDisplay);
+  const [updatedBalance, setUpdatedBalance] = useState(balance);
+
+  useEffect(() => {
+    if (gameHistory.length > 0) {
+      setUpdatedBalance(balance);
+    }
+  }, [gameHistory, balance]);
+
+  const handleShowHistory = () => {
+    setShowHistory(true);
+  };
+
+  const handleCloseHistory = () => {
+    setShowHistory(false);
+  };
+
+  const handleClearHistory = () => {
+    dispatch(clearHistory());
+    setShowHistory(false);
+  };
 
   return (
     <div className='home-page'>
       <div className='content-overlay'></div>
-      <h2>Your balance: {balanceToDisplay}</h2>
-
-      <h3>Game History:</h3>
-      {gameHistory.length > 0 ? (
-        <ul>
-          {gameHistory.map((game, index) => (
-            <li key={index}>
-              Game: {game.game}, Result: {game.result}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No games played yet.</p>
-      )}
+      <h2>Your balance: {updatedBalance}</h2>
+      <button className='btn-color btn-history' onClick={handleShowHistory}>View History</button>
       <Link to="/coin-game">Coin Game</Link>
       <br />
       <Link to="/door-game">Door Game</Link>
       <br />
       <Link to="/number-game">Number Game</Link>
       <br />
-      <Link to="/">Go back to Login Page</Link>
+      <Link className='btn-color' to='/add-funds'>Add Funds</Link>
+      <br />
+      <Link className='btn-color' to='/'>Log Out</Link>
+
+      <Modal isOpen={showHistory} handleClose={handleCloseHistory}>
+        {gameHistory.length > 0 ? (
+          <ul>
+            {gameHistory.map((game, index) => (
+              <li key={index}>
+                Game: {game.game}, Result: {game.result}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No games played yet.</p>
+        )}
+        <button onClick={handleClearHistory}>Clear History</button>
+      </Modal>
     </div>
   );
 }
