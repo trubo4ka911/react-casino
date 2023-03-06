0b0a2f4e8c67f9b3d48bfa83ed59538bcf13a464
Anna Trubnikova <trubo4ka911@gmail.com>
2023-03-06 21:33:49 +0200

    Create Modal.jsx

---
+++ src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../sass/Modal.scss';
+
+function Modal({ isOpen, handleClose, children }) {
+  return (
+    <>
+      {isOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <button className="modal-close" onClick={handleClose}>
+              X
+            </button>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Modal;
