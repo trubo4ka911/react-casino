81378246d0b3ee3a958e446c3d82214a3e3e8781
Anna Trubnikova <trubo4ka911@gmail.com>
2023-03-09 12:22:47 +0200

    Update WinnerPage.jsx

--- src/components/WinnerPage.jsx
+++ src/components/WinnerPage.jsx
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import "../sass/components/WinnerPage.scss";
 
 function WinnerPage() {
   const balance = useSelector((state) => state.balance.currentBalance);
 
   return (
-    <div>
+    <div className='winner-page'>
       <h2>Congratulations, you've doubled your balance!</h2>
       <p>Your new balance is {balance}.</p>
       <Link to="/home-page">Go to Home</Link>
