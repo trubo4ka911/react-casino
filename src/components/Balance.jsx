b0c2cafac5324573040b5df38a8833c863cb80ae
Anna Trubnikova <trubo4ka911@gmail.com>
2023-03-09 13:07:46 +0200

    Delete Balance.jsx

--- src/components/Balance.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-
-function Balance() {
-  const balance = useSelector((state) => state.balance);
-
-  return <div>Balance: {balance}</div>;
-}
-
-export default Balance;
